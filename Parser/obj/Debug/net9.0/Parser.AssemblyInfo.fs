b7df9faf816fa1ddc65725f16c98d6a6ba71cc52
JWood2001 <jewood2@shockers.wichita.edu>
2025-05-05 10:28:56 -0500

    Fix webhosting to run UI

--- Parser/obj/Debug/net9.0/Parser.AssemblyInfo.fs
+++ Parser/obj/Debug/net9.0/Parser.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Parser")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0589708e8c0709035e9b94c8c9ac2a8335d798a5")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7b11e1ffe82dc5ade46025ae05a3009bae1365")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Parser")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Parser")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
--- WebApp/Program.fs
+++ WebApp/Program.fs
@@ -2,73 +2,74 @@
 
 module WebApp.Program
 
-open System.Threading.Tasks
-open Microsoft.AspNetCore.Builder
-open Microsoft.Extensions.Hosting
-
+open System
 open Giraffe
 open Giraffe.ViewEngine
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Http
 
 open Parser.AST
 open Parser.Parsing
 
 /// Recursively render an Expr as a nested <ul class="ast">
 let rec renderList (expr: Expr) : XmlNode =
-    // Build any child subtrees
     let children =
         match expr with
         | IntLit _ | Var _    -> []
-        | Lambda(_, body)     -> [ renderList body ]
+        | Lambda(_, b)        -> [ renderList b ]
         | App(f, x)           -> [ renderList f; renderList x ]
         | Let(_, v, b)        -> [ renderList v; renderList b ]
 
-    // Create a UL node whose first item is this expr, then its children
-    ul [ _class "ast" ] (
+    ul [ _class "ast" ] [
+      li [] (
         [ span [] [ str (sprintf "%O" expr) ] ] @ children
-    )
+      )
+    ]
 
-/// The main HTTP handler
+/// The main Giraffe HTTP handler
 let webApp : HttpHandler =
     choose [
-        GET  >=> htmlFile "Views/index.html"
-        POST >=> fun next ctx -> task {
-            // Bind form data
-            let! form = ctx.BindFormAsync()
-            let code = form.["code"].ToString()
-
-            match parse code with
-            | Ok ast ->
-                // Render and wrap in an HTML document
-                let view = renderList ast
-                let htmlDoc =
-                    html [] [
-                        head [] [
-                            title [] [ str "MicroML AST Visualizer" ]
-                            link [ _rel "stylesheet"; _href "/styles.css" ]
-                        ]
-                        body [] [ view ]
-                    ]
-                let htmlStr = RenderView.AsString.htmlDocument htmlDoc
-                return! htmlString htmlStr next ctx
-
-            | Error err ->
-                return! htmlString (sprintf "<p class='error'>Parse error: %s</p>" err) next ctx
+      GET  >=> htmlFile "Views/index.html"
+      POST >=> fun next ctx ->
+        task {
+           // Read the form collection (known IFormCollection type)
+          let! form = ctx.Request.ReadFormAsync()
+          // Now form has type IFormCollection, so indexing works
+          let code = form.["code"].ToString()
+          match parse code with
+          | Ok ast ->
+              let view = renderList ast
+              // Wrap in full HTML
+              let doc =
+                html [] [
+                  head [] [
+                    title [] [ str "MicroML AST Visualizer" ]
+                    link [ _rel "stylesheet"; _href "/styles.css" ]
+                  ]
+                  body [] [ view ]
+                ]
+              return! htmlString (RenderView.AsString.htmlDocument doc) next ctx
+          | Error e ->
+              return! htmlString $"<p class='error'>Parse error: {e}</p>" next ctx
         }
     ]
 
 [<EntryPoint>]
 let main args =
-    Host.CreateDefaultBuilder(args)
-      .ConfigureWebHostDefaults(fun webBuilder ->
-          webBuilder.UseUrls("http://localhost:5000") |> ignore
-          webBuilder.Configure(fun appBuilder ->
-              appBuilder.UseStaticFiles() |> ignore
-              appBuilder.UseGiraffe webApp   |> ignore
-          ) |> ignore
-      )
-      .Build()
-      .Run()
+    // Create the WebApplicationBuilder
+    let builder = WebApplication.CreateBuilder(args)
+    // Add Giraffe services
+    builder.Services.AddGiraffe() |> ignore
+
+    let app = builder.Build()
+
+    // Serve wwwroot static files and use Giraffe
+    app.UseStaticFiles() |> ignore
+    app.UseGiraffe webApp
+
+    // Run on localhost:5000
+    app.Run("http://localhost:5000")
+
     0
-
-
-
